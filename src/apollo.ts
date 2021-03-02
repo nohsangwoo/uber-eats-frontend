06da1265108189f6d5adcda0ded499f1af56d650
nohsangwoo <nsgr12@naver.com>
2021-03-02 21:21:31 +0900

    change url

--- src/apollo.ts
+++ src/apollo.ts
@@ -4,11 +4,11 @@ import {
   makeVar,
   createHttpLink,
   split,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { WebSocketLink } from "@apollo/client/link/ws";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { LOCALSTORAGE_TOKEN } from "./constants";
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { LOCALSTORAGE_TOKEN } from './constants';
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
@@ -16,29 +16,29 @@ export const authTokenVar = makeVar(token);
 
 const wsLink = new WebSocketLink({
   uri:
-    process.env.NODE_ENV === "production"
-      ? "wss://nuber-eats-backend.herokuapp.com/graphql"
+    process.env.NODE_ENV === 'production'
+      ? 'wss://uber-eats-clone-noh.herokuapp.com//graphql'
       : `ws://localhost:4000/graphql`,
   options: {
     reconnect: true,
     connectionParams: {
-      "x-jwt": authTokenVar() || "",
+      'x-jwt': authTokenVar() || '',
     },
   },
 });
 
 const httpLink = createHttpLink({
   uri:
-    process.env.NODE_ENV === "production"
-      ? "https://nuber-eats-backend.herokuapp.com/graphql"
-      : "http://localhost:4000/graphql",
+    process.env.NODE_ENV === 'production'
+      ? 'https://uber-eats-clone-noh.herokuapp.com//graphql'
+      : 'http://localhost:4000/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      "x-jwt": authTokenVar() || "",
+      'x-jwt': authTokenVar() || '',
     },
   };
 });
@@ -47,8 +47,8 @@ const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
     );
   },
   wsLink,

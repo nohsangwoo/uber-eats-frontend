6ff9def55b62e9aaa013cadfbee9b40ee8714f38
nohsangwoo <nsgr12@naver.com>
2021-03-03 14:08:06 +0900

    add category

--- src/pages/owner/my-restaurants.tsx
+++ src/pages/owner/my-restaurants.tsx
@@ -1,7 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Restaurant } from '../../components/restaurant';
 import { RESTAURANT_FRAGMENT } from '../../fragments';
 import { myRestaurants } from '../../__generated__/myRestaurants';
@@ -21,34 +21,62 @@ export const MY_RESTAURANTS_QUERY = gql`
 
 export const MyRestaurants = () => {
   const { data } = useQuery<myRestaurants>(MY_RESTAURANTS_QUERY);
+  const CONSTANTS = {
+    RESTAURANT: 'RESTAURANT',
+    CATEGORIES: 'CATEGORIES',
+  };
+  const history = useHistory();
+
+  const moveRoute = (CONSTANT: string) => {
+    console.log('gggg', CONSTANT);
+    if (CONSTANT === CONSTANTS.RESTAURANT) {
+      history.push('/add-restaurant');
+    } else if (CONSTANT === CONSTANTS.CATEGORIES) {
+      history.push('/add-category');
+    }
+  };
   return (
     <div>
       <Helmet>
         <title>My Restaurants | Nuber Eats</title>
       </Helmet>
       <div className="max-w-screen-2xl mx-auto mt-32">
-        <h2 className="text-4xl font-medium mb-10">My Restaurants</h2>
+        <h2 className="flex felx-row justify-center w-full text-4xl font-medium mb-10 border-t-4 border-b-4 border-black py-2">
+          My Restaurants
+        </h2>
 
         {data?.myRestaurants.ok &&
         data.myRestaurants.restaurants.length === 0 ? (
           <>
-            <h4 className="text-xl mb-5">You have no restaurants.</h4>
-            <Link
-              className="text-lime-600 hover:underline"
-              to="/add-restaurant"
-            >
-              Create one &rarr;
-            </Link>
+            <h4 className="text-xl mb-5">You have no restaurants</h4>
+            <div>Create one &rarr;</div>
           </>
         ) : (
           <>
-            <h4 className="text-xl mb-5">Create more restaurants.</h4>
-            <Link
-              className="text-lime-600 hover:underline"
-              to="/add-restaurant"
-            >
-              Create one &rarr;
-            </Link>
+            <div className="flex flex-row justify-around border-t-2 border-black">
+              <div
+                onClick={() => {
+                  moveRoute(CONSTANTS.RESTAURANT);
+                }}
+                className="transition-all w-full text-xl mb-5 cursor-pointer hover:border-black border-2"
+              >
+                <h4 className="">Create more restaurants</h4>
+                <div className="text-lime-600 hover:underline">
+                  Create one &rarr;
+                </div>
+              </div>
+              <div
+                onClick={() => {
+                  moveRoute(CONSTANTS.CATEGORIES);
+                }}
+                className="transition-all w-full text-xl mb-5 cursor-pointer hover:border-black border-2"
+              >
+                <h4 className="">Create more categories</h4>
+                <div className="text-lime-600 hover:underline">
+                  Create one &rarr;
+                </div>
+              </div>
+            </div>
 
             <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
               {data?.myRestaurants.restaurants.map(restaurant => (
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,55 +1,57 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Header } from "../components/header";
-import { useMe } from "../hooks/useMe";
-import { NotFound } from "../pages/404";
-import { Restaurants } from "../pages/client/restaurants";
-import { Search } from "../pages/client/search";
-import { Category } from "../pages/client/category";
-import { ConfirmEmail } from "../pages/user/confirm-email";
-import { EditProfile } from "../pages/user/edit-profile";
-import { Restaurant } from "../pages/client/restaurant";
-import { MyRestaurants } from "../pages/owner/my-restaurants";
-import { MyRestaurant } from "../pages/owner/my-restaurant";
-import { AddRestaurant } from "../pages/owner/add-restaurants";
-import { AddDish } from "../pages/owner/add-dish";
-import { Order } from "../pages/order";
-import { Dashboard } from "../pages/driver/dashboard";
-import { UserRole } from "../__generated__/globalTypes";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Header } from '../components/header';
+import { useMe } from '../hooks/useMe';
+import { NotFound } from '../pages/404';
+import { Restaurants } from '../pages/client/restaurants';
+import { Search } from '../pages/client/search';
+import { Category } from '../pages/client/category';
+import { ConfirmEmail } from '../pages/user/confirm-email';
+import { EditProfile } from '../pages/user/edit-profile';
+import { Restaurant } from '../pages/client/restaurant';
+import { MyRestaurants } from '../pages/owner/my-restaurants';
+import { MyRestaurant } from '../pages/owner/my-restaurant';
+import { AddRestaurant } from '../pages/owner/add-restaurants';
+import { AddDish } from '../pages/owner/add-dish';
+import { Order } from '../pages/order';
+import { Dashboard } from '../pages/driver/dashboard';
+import { UserRole } from '../__generated__/globalTypes';
+import AddCategory from '../pages/owner/add-category';
 
 const clientRoutes = [
   {
-    path: "/",
+    path: '/',
     component: <Restaurants />,
   },
   {
-    path: "/search",
+    path: '/search',
     component: <Search />,
   },
   {
-    path: "/category/:slug",
+    path: '/category/:slug',
     component: <Category />,
   },
   {
-    path: "/restaurants/:id",
+    path: '/restaurants/:id',
     component: <Restaurant />,
   },
 ];
 
 const commonRoutes = [
-  { path: "/confirm", component: <ConfirmEmail /> },
-  { path: "/edit-profile", component: <EditProfile /> },
-  { path: "/orders/:id", component: <Order /> },
+  { path: '/confirm', component: <ConfirmEmail /> },
+  { path: '/edit-profile', component: <EditProfile /> },
+  { path: '/orders/:id', component: <Order /> },
 ];
 
 const restaurantRoutes = [
-  { path: "/", component: <MyRestaurants /> },
-  { path: "/add-restaurant", component: <AddRestaurant /> },
-  { path: "/restaurants/:id", component: <MyRestaurant /> },
-  { path: "/restaurants/:restaurantId/add-dish", component: <AddDish /> },
+  { path: '/', component: <MyRestaurants /> },
+  { path: '/add-restaurant', component: <AddRestaurant /> },
+  { path: '/restaurants/:id', component: <MyRestaurant /> },
+  { path: '/restaurants/:restaurantId/add-dish', component: <AddDish /> },
+  { path: '/add-category', component: <AddCategory /> },
 ];
 
-const driverRoutes = [{ path: "/", component: <Dashboard /> }];
+const driverRoutes = [{ path: '/', component: <Dashboard /> }];
 
 export const LoggedInRouter = () => {
   const { data, loading, error } = useMe();
@@ -65,24 +67,24 @@ export const LoggedInRouter = () => {
       <Header />
       <Switch>
         {data.me.role === UserRole.Client &&
-          clientRoutes.map((route) => (
+          clientRoutes.map(route => (
             <Route exact key={route.path} path={route.path}>
               {route.component}
             </Route>
           ))}
         {data.me.role === UserRole.Owner &&
-          restaurantRoutes.map((route) => (
+          restaurantRoutes.map(route => (
             <Route exact key={route.path} path={route.path}>
               {route.component}
             </Route>
           ))}
         {data.me.role === UserRole.Delivery &&
-          driverRoutes.map((route) => (
+          driverRoutes.map(route => (
             <Route exact key={route.path} path={route.path}>
               {route.component}
             </Route>
           ))}
-        {commonRoutes.map((route) => (
+        {commonRoutes.map(route => (
           <Route key={route.path} path={route.path}>
             {route.component}
           </Route>

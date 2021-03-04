2db48a917540e46a427312efc3f7fef364a28c8b
nohsangwoo <nsgr12@naver.com>
2021-03-04 18:50:51 +0900

    category page

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,6 +1,8 @@
 import { gql, useQuery } from '@apollo/client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { useLocation, useParams } from 'react-router-dom';
+import { Restaurant } from '../../components/restaurant';
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { category, categoryVariables } from '../../__generated__/category';
 
@@ -28,18 +30,67 @@ interface ICategoryParams {
 }
 
 export const Category = () => {
-  const params = useParams<ICategoryParams>();
+  const [page, setPage] = useState(1);
+  const { slug } = useParams<ICategoryParams>();
   const { data, loading } = useQuery<category, categoryVariables>(
     CATEGORY_QUERY,
     {
       variables: {
         input: {
           page: 1,
-          slug: params.slug,
+          slug: slug,
         },
       },
     }
   );
-  console.log(loading, data);
-  return <h1>Category</h1>;
+  const onNextPageClick = () => setPage(current => current + 1);
+  const onPrevPageClick = () => setPage(current => current - 1);
+
+  return (
+    <div className="">
+      <Helmet>
+        <title>{`${slug} | Uber Eats`}</title>
+      </Helmet>
+      {!loading && (
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-96">
+          <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+            {data?.category?.restaurants?.map(restaurant => (
+              <Restaurant
+                key={restaurant.id}
+                id={restaurant.id + ''}
+                coverImg={restaurant.coverImg}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className="focus:outline-none font-medium text-2xl"
+              >
+                &larr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <span>
+              Page {page} of {data?.category?.totalPages}
+            </span>
+            {page !== data?.category?.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className="focus:outline-none font-medium text-2xl"
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
